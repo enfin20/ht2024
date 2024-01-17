82a768eeff2f2ff276d31af90986a6653c384fa7
enfin20 <96887337+enfin20@users.noreply.github.com>
2024-01-17 18:43:46 +0100

    icons

--- src/lib/LeafletMap.svelte
+++ src/lib/LeafletMap.svelte
@@ -8,24 +8,16 @@
     if (typeof window != "undefined") {
       const leaflet = await import("leaflet");
       var greenIcon = new leaflet.Icon({
-        iconUrl:
-          "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
-        shadowUrl:
-          "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41],
+        iconUrl: "/images/starred.png",
+        iconSize: [32, 32],
+        iconAnchor: [16, 32],
+        popupAnchor: [1, -24],
       });
       var redIcon = new leaflet.Icon({
-        iconUrl:
-          "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
-        shadowUrl:
-          "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41],
+        iconUrl: "/images/camping-tent.png",
+        iconSize: [32, 32],
+        iconAnchor: [16, 32],
+        popupAnchor: [1, -24],
       });
       map = leaflet.map(mapElement).setView([45.17334, 6.65169], 12);
       leaflet
